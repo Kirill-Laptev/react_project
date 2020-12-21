5ed06a59534ac8f253cd68ccce6d70d46f29b0ee
Kirill <laptevkirill.wwc@gmail.com>
2020-12-21 11:09:57 +0300

    #85. React hooks. useEffect()

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 const ProfileStatusWithHooks = (props) => {
 
-    // let stateWithSetState = useState(false);
-    // let editMode = stateWithSetState[0];
-    // let setEditMode = stateWithSetState[1];
-
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
+
+
+    useEffect(() => {
+        debugger;
+        setStatus(props.status)
+    }, [props.status])
+
 
     const activateMode = () => {
         setEditMode(true)
