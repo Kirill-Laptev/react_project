6dcbd2c220df5e3fbf9f8ca8a709657d3962306c
Kirill <laptevkirill.wwc@gmail.com>
2020-12-31 14:32:59 +0300

    Updates

---
+++ precache-manifest.d5a02d075d0547eb72ad56e564717f8b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1786f2ba96667b662695c0e5a20f7b9e",
+    "url": "/first_project_repeat/index.html"
+  },
+  {
+    "revision": "4897f232460dd3ae9cb4",
+    "url": "/first_project_repeat/static/css/3.74343c10.chunk.css"
+  },
+  {
+    "revision": "af3d58a86f28701738de",
+    "url": "/first_project_repeat/static/css/4.432cf3a2.chunk.css"
+  },
+  {
+    "revision": "7ad189b5155a32ad5eff",
+    "url": "/first_project_repeat/static/css/main.403d6c8b.chunk.css"
+  },
+  {
+    "revision": "d95aa5132f629f827546",
+    "url": "/first_project_repeat/static/js/2.8ce01a11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/first_project_repeat/static/js/2.8ce01a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4897f232460dd3ae9cb4",
+    "url": "/first_project_repeat/static/js/3.79ba5bc8.chunk.js"
+  },
+  {
+    "revision": "af3d58a86f28701738de",
+    "url": "/first_project_repeat/static/js/4.0ff5de7d.chunk.js"
+  },
+  {
+    "revision": "7ad189b5155a32ad5eff",
+    "url": "/first_project_repeat/static/js/main.032e2f5f.chunk.js"
+  },
+  {
+    "revision": "3841442619132449e7fb",
+    "url": "/first_project_repeat/static/js/runtime-main.0724a472.js"
+  },
+  {
+    "revision": "27fc27b682d7147fc0935299c8467922",
+    "url": "/first_project_repeat/static/media/Spinner-1s-200.27fc27b6.svg"
+  },
+  {
+    "revision": "f9fcc79625c73f393ce6377602a6f015",
+    "url": "/first_project_repeat/static/media/user.f9fcc796.png"
+  }
+]);
